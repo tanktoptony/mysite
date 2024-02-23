0a74e9309f8973ab3c33a82f06f5f01f7da46b9a
tanktoptony <anthony.jarina@gmail.com>
2024-02-22 22:12:38 -0600

    functional blog, category, and blog index views as well as basic styling.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.blog_index, name="blog_index"),
+    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,3 +7,22 @@ def blog_index(request):
         "posts": posts,
     }
     return render(request, "blog/index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+    return render(request, "blog/detail.html", context)
