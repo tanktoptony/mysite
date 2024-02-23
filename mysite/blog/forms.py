f486b6337c4c37f90811c0e78e68353f0d7b605e
tanktoptony <anthony.jarina@gmail.com>
2024-02-23 00:10:13 -0600

    basic blog functionality complete

---
+++ mysite/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+class CoommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your Name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "leave a comment"}
+        )
+    )
