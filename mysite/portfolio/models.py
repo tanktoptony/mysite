ad2a5186d1e8852701a60fa2f9b3ea818eea9209
tanktoptony <anthony.jarina@gmail.com>
2024-02-28 14:29:34 -0600

    working on adding portfolio project models

--- mysite/portfolio/models.py
+++ mysite/portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="projects")
+    description = models.TextField()
